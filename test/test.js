bd5d8c37cbff8f5f5ded27cefa59364bab2d763c
geekplux <geekplux@qq.com>
2017-03-16 01:03:26 +0800

    render linechart; refactor test; add js-yaml

--- test/test.js
+++ test/test.js
@@ -18,37 +18,23 @@ console.log('language javascript')
 \`\`\`
 
 \`\`\`vis
-{
-  "description": "A simple bar chart with embedded data that uses a filter and calculate.",
-  "data": {
-    "values": [
-      {"a": "A","b": 28},
-      {"a": "B","b": 55},
-      {"a": "C","b": 43},
-      {"a": "G","b": 19},
-      {"a": "H","b": 87},
-      {"a": "I","b": 52},
-      {"a": "D","b": 91},
-      {"a": "E","b": 81},
-      {"a": "F","b": 53}
-    ]
-  },
-  "transform": {
-    "calculate": [{"field": "b2","expr": "2*datum.b"}],
-    "filter": "datum.b2 > 60"
-  },
-  "mark": "bar",
-  "encoding": {
-    "y": {"field": "b2", "type": "quantitative"},
-    "x": {"field": "a", "type": "ordinal"}
-  }
-}
+layout: bar
+data: [
+  { key: 0, value: 5 },
+  { key: 1, value: 4 },
+  { key: 2, value: 7 },
+  { key: 3, value: 2 },
+  { key: 4, value: 4 },
+  { key: 5, value: 8 },
+  { key: 6, value: 3 },
+  { key: 7, value: 6 }
+]
 \`\`\`
 `);
 
 console.log(testStr);
 
-fs.writeFile('test.html', testStr, (err) => {
+fs.writeFile('./test/test.html', testStr, (err) => {
   if (err) throw err;
   console.log('test saved!');
 });
