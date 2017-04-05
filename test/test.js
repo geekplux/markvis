179b3a588bb7412cf9dcb134123155db3be8f14a
geekplux <geekplux@qq.com>
2017-04-05 15:26:36 +0800

    add barchart render

--- test/test.js
+++ test/test.js
@@ -30,6 +30,22 @@ data: [
   { key: 7, value: 6 }
 ]
 \`\`\`
+
+
+\`\`\`vis
+layout: line
+data: [
+  { key: 0, value: 5 },
+  { key: 1, value: 4 },
+  { key: 2, value: 7 },
+  { key: 3, value: 2 },
+  { key: 4, value: 4 },
+  { key: 5, value: 8 },
+  { key: 6, value: 3 },
+  { key: 7, value: 6 }
+]
+\`\`\`
+
 `);
 
 console.log(testStr);
