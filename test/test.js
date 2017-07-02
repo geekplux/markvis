3e64383b337eb4f130f3fac648705b353ab9f6d5
geekplux <geekplux@qq.com>
2017-07-02 10:36:08 +0800

    add d3node-piechart

--- test/test.js
+++ test/test.js
@@ -46,6 +46,20 @@ data: [
 ]
 \`\`\`
 
+
+\`\`\`vis
+layout: pie
+data: [
+  { key: 0, value: 5 },
+  { key: 1, value: 4 },
+  { key: 2, value: 7 },
+  { key: 3, value: 2 },
+  { key: 4, value: 4 },
+  { key: 5, value: 8 },
+  { key: 6, value: 3 },
+  { key: 7, value: 6 }
+]
+\`\`\`
 `)
 
 console.log(testStr)
